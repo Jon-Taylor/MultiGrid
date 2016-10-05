f2c09695b78ac2e4e5b906859066a1ffb19a3a78
Jon-Taylor <jonathan.taylor@esss.se>
2016-10-05 16:43:31 +0200

    added a definition file for 1/2 the MG detector set with the geometry of CNCS

--- cncs_Mantid.py
+++ cncs_Mantid.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def histEventList(evlist):
     bins = np.histogram(evlist[:, 1], 500)
-    binnedData=np.zeros([500,8*16*48])
-    for i in range(8*16*48):
+    binnedData=np.zeros([500,4*16*48])
+    for i in range(4*16*48):
         ind = evlist[:, 0] == i
         tmp=evlist[ind,1]
         
@@ -18,5 +18,5 @@ evlst=np.load('/Users/jonathantaylor/PycharmProjects/MultiGrid/Eventlist.dat.npy
 
 bins,binneddata=histEventList(evlst)
 print binneddata
-test=CreateWorkspace(bins[1],np.transpose(binneddata),NSpec=8*16*48)
-
+test=CreateWorkspace(bins[1],np.transpose(binneddata),NSpec=4*16*48,UnitX='tof')
+CreateWorkspace(
